8fec7e95d5e17f9e9d67dd8a5635785253b88949
pooryarajaee <pooryarajaee@gmail.com>
2016-04-19 18:30:36 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -227,12 +227,13 @@ function create_config( )
     "broadcast",
     "invite",
     "all",
+    "plugins",
     "leave_ban",
 	"supergroup",
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {130470753,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
